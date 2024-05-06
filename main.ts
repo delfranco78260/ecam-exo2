0ff925fbe82bc51cb411ef6174d978c91a927c0f
delfranco78260 <76877535+delfranco78260@users.noreply.github.com>
2024-05-06 13:45:51 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+basic.forever(function () {
+    huskylens.request()
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.Yes)
+        music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+        basic.pause(100)
+    } else {
+        basic.showIcon(IconNames.No)
+        music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+        basic.pause(100)
+    }
+})
